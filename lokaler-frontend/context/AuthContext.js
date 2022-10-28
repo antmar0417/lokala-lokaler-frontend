3050451403e61a8146b351f58f55ef7c7202306b
antmar0417 <tonyboy@live.se>
2022-10-28 19:05:32 +0200

    fixed logout user

--- lokaler-frontend/context/AuthContext.js
+++ lokaler-frontend/context/AuthContext.js
@@ -15,7 +15,9 @@ export const AuthProvider = ({ children }) => {
 
   const router = useRouter();
 
-  // useEffect(() => checkUserLoggedIn(), []);
+  useEffect(() => {
+    checkUserLoggedIn();
+  }, []);
 
   // --------- Register user ---------
   const register = async (user) => {
@@ -40,7 +42,7 @@ export const AuthProvider = ({ children }) => {
     if (res.ok) {
       // --- Setting that user from login ---
       setUser(data.user);
-      router.push("/");
+      router.push("/konto/instrumentfodral");
     } else {
       toast.error(data.message);
       // setError(null);
@@ -49,12 +51,26 @@ export const AuthProvider = ({ children }) => {
 
   // ------------- Logout user -------------
   const logout = async () => {
-    console.log("logout");
+    const res = await fetch(`${NEXT_URL}/api/logout`, {
+      method: "POST",
+    });
+
+    if (res.ok) {
+      setUser(null);
+      router.push("/");
+    }
   };
 
   // ------ Check if user is logged in ------
   const checkUserLoggedIn = async (user) => {
-    console.log(user);
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
   };
 
   return (
---
+++ lokaler-frontend/pages/api/user.js
@@ -0,0 +1,34 @@
+import cookie from "cookie";
+import { API_URL } from "@/config/index";
+
+// --------- Checking if the user is logged in ---------
+export default async (req, res) => {
+  if (req.method === "GET") {
+    if (!req.headers.cookie) {
+      res.status(403).json({ message: "Not Authorized" });
+      return;
+    }
+
+    // ------- Parse the cookie and get the token -------
+    const { token } = cookie.parse(req.headers.cookie);
+
+    // ------- Passing the token to strapi route -------
+    const strapiRes = await fetch(`${API_URL}/api/users/me`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const user = await strapiRes.json();
+
+    if (strapiRes.ok) {
+      res.status(200).json({ user });
+    } else {
+      res.status(403).json({ message: "User forbidden" });
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({ message: `Method ${req.method} not allowed` });
+  }
+};
