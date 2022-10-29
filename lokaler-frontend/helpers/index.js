9f88fc255bb9ca7b817ba3707b90540ab84c703f
antmar0417 <tonyboy@live.se>
2022-10-29 14:33:03 +0200

    showing the user premises

---
+++ lokaler-frontend/helpers/index.js
@@ -0,0 +1,6 @@
+import cookie from "cookie";
+
+export function parseCookies(req) {
+  // ------- Parse the cookie and get the token if request = true -------
+  return cookie.parse(req ? req.headers.cookie || "" : "");
+}
--- lokaler-frontend/pages/konto/instrumentfodral.js
+++ lokaler-frontend/pages/konto/instrumentfodral.js
@@ -1,10 +1,68 @@
+import { parseCookies } from "@/helpers/index";
+import { useRouter } from "next/router";
+import { API_URL } from "@/config/index";
+// default exports
 import Layout from "@/components/Layout";
 
-export default function DashboardPage() {
+export default function DashboardPage({ premises, token }) {
+  // ------------ logging the premises ------------
+  // console.log(premises);
+  const router = useRouter();
+
+  const deletePremise = async (id) => {
+    if (confirm("Är du säker?")) {
+      const res = await fetch(`${API_URL}/api/premises/${lkl.id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message);
+      } else {
+        router.reload();
+      }
+    }
+  };
+
   return (
-    <Layout title="User Dashboard">
-      <h1>Dashboard</h1>
-      <h3>My Events</h3>
+    <Layout title="Instrumentfodral">
+      <div className="">
+        <h1>Instrumentfodral</h1>
+        <h3>Mina Lokaler</h3>
+
+        {premises.map((lkl) => (
+          <h1 key={lkl.id} lkl={lkl}>
+            {lkl.title}
+          </h1>
+        ))}
+      </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  // Logging the token
+  console.log(token);
+
+  const res = await fetch(`${API_URL}/api/premises/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const premises = await res.json();
+
+  return {
+    props: {
+      premises,
+      token,
+    },
+  };
 }
