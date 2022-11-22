69751f40143a52d28b540e4e49e902b6642d9e87
antmar0417 <tonyboy@live.se>
2022-11-22 12:23:30 +0100

    Update [slug].js

--- pages/lokaler/[slug].js
+++ pages/lokaler/[slug].js
@@ -47,7 +47,7 @@ export default function PremisePage({ lkl }) {
           </div>
 
           <div className="flex flex-row justify-center items-center text-white">
-            <div className="xxs:w-[194px] md:w-[860px] mt-[40px] flex flex-col justify-left items-left">
+            <div className="xxs:w-[234px] md:w-[860px] mt-[40px] flex flex-col justify-left items-left">
               <p className="text-[24px] font-bold">Beskrivning</p>
               <p>{attributes.description}</p>
             </div>
@@ -55,7 +55,7 @@ export default function PremisePage({ lkl }) {
 
           <div className="flex flex-row justify-center items-center text-white">
             <div className="w-[860px] mt-[40px] flex xxs:flex-col xxs:items-center xxs:justify-center md:flex-row md:items-start md:justify-between text-white">
-              <div className=" xxs:w-[198px] flex flex-col ">
+              <div className=" xxs:w-[234px] flex flex-col ">
                 <p className=" text-[24px] font-bold">Address</p>
                 <p>{attributes.address}</p>
 
@@ -66,7 +66,7 @@ export default function PremisePage({ lkl }) {
                 <p>{`${attributes.price} kr`}</p>
               </div>
 
-              <div className="xxs:w-[198px] flex flex-col ">
+              <div className="xxs:w-[234px] md:w-[250px] flex flex-col ">
                 <p className=" text-[24px] font-bold">Kontakt</p>
 
                 <p>
